09b86af40b363b06b6586610e61eec99204003a1
romaintailhurat <romain.tailhurat@gmail.com>
2023-04-03 14:40:11 +0200

    init.sh

---
+++ init.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+WORK_DIR = "/home/onyxia/work"
+REPO_URL = "https://github.com/romaintailhurat/Bowie.git"
+
+curl -sSL https://install.python-poetry.org | python3 -
+
+export PATH="/home/onyxia/.local/bin:$PATH"
+
+git clone $REPO_URL $WORK_DIR
+
+cd /home/onyxia/work/Bowie
+
+git switch pages
+
+poetry install
+
